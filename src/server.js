aba49058637e3c132343e6912424163c8b60d4a2
Michaela Blackham <Michaelablackham@gmail.com>
2017-09-04 19:16:40 -0600

    revisions to clean up files

--- src/server.js
+++ src/server.js
@@ -24,106 +24,10 @@ mongoose.Promise = global.Promise;
 // app.use('/blog-posts', blogPostsRouter);
 
 app.get('/posts', (req, res) => {
-  Post
-    .find()
-    .exec()
-    .then(posts => {
-      res.json(posts.map(post => post.apiRepr()));
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went terribly wrong'});
-    });
-});
-
-app.get('/posts/:id', (req, res) => {
-  Post
-    .findById(req.params.id)
-    .exec()
-    .then(post => res.json(post.apiRepr()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went horribly awry'});
-    });
-});
-
-app.post('/posts', (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-
-  Post
-    .create({
-      title: req.body.title,
-      content: req.body.content,
-      author: req.body.author
-    })
-    .then(blogPost => res.status(201).json(blogPost.apiRepr()))
-    .catch(err => {
-        console.error(err);
-        res.status(500).json({error: 'Something went wrong'});
-    });
-
+  res.json(Post.get());
 });
 
 
-app.delete('/posts/:id', (req, res) => {
-  Post
-    .findByIdAndRemove(req.params.id)
-    .exec()
-    .then(() => {
-      res.status(204).json({message: 'success'});
-    })
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({error: 'something went terribly wrong'});
-    });
-});
-
-
-app.put('/posts/:id', (req, res) => {
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    res.status(400).json({
-      error: 'Request path id and request body id values must match'
-    });
-  }
-
-  const updated = {};
-  const updateableFields = ['title', 'content', 'author'];
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      updated[field] = req.body[field];
-    }
-  });
-
-  Posts
-    .findByIdAndUpdate(req.params.id, {$set: updated}, {new: true})
-    .exec()
-    .then(updatedPost => res.status(201).json(updatedPost.apiRepr()))
-    .catch(err => res.status(500).json({message: 'Something went wrong'}));
-});
-
-
-app.delete('/:id', (req, res) => {
-  Posts
-    .findByIdAndRemove(req.params.id)
-    .exec()
-    .then(() => {
-      console.log(`Deleted blog post with id \`${req.params.ID}\``);
-      res.status(204).end();
-    });
-});
-
-
-app.use('*', function(req, res) {
-  res.status(404).json({message: 'Not Found'});
-});
 
 //used for both runServer and closeServer
 //Accesses the same server object
